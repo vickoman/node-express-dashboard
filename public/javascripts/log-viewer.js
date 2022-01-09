ca3e63c04b8c4a3f6d27ca580da8bc15d6f4d962
vickoman <vicmandlagasca@gmail.com>
2022-01-08 19:57:52 -0500

    Fix message

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,7 +2,7 @@ const connection = new WebSocket("ws://localhost:3000");
 const logWindow =  document.querySelector("#log-window");
 
 connection.onopen = () => {
-    connection.send("Hello from the cllient!");
+    connection.send("Hello from the client!");
 };
 
 connection.onmessage = (event) => {
