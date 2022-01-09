289ce342bc6506df058166b953ffb19faa0977bb
vickoman <vicmandlagasca@gmail.com>
2022-01-08 20:07:02 -0500

    change query selector to getElementById

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,6 +1,6 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow =  document.querySelector("#log-window");
-const filePath = document.querySelector("#logFilePath");
+const filePath = document.getElementById("logFilePath");
 
 connection.onopen = () => {
     connection.send("Hello from the client!");
