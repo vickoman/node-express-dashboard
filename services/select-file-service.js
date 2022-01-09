440f2b2a03b99eb87c63398ecc8faf76fd10267c
vickoman <vicmandlagasca@gmail.com>
2022-01-08 20:48:00 -0500

    fs isdirectory

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
 function getDirectoryContents(files, currentDir, query) {
 }
 
 function isDirectory(currentDir, file) {
+    const fileInfo = path.join(currentDir, file);
 }
 
 function readDir(currentDir, res, query) {
