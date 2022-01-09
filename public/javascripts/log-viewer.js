6f8f825f477e8a4ae86863bb515caa1abae11bfb
vickoman <vicmandlagasca@gmail.com>
2022-01-08 20:13:00 -0500

    Fix reference to inout hidden

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,6 +1,6 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow =  document.querySelector("#log-window");
-const filePath = document.getElementById("logFilePath");
+const filePath = document.getElementById("logFilePath").value;
 
 connection.onopen = () => {
     connection.send("Hello from the client!");
