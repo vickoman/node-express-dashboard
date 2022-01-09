9e7c81c4094e9c7acd3b64201830072d412b5779
vickoman <vicmandlagasca@gmail.com>
2022-01-08 20:06:13 -0500

    add logFile reference in log viewer file

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,5 +1,6 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow =  document.querySelector("#log-window");
+const filePath = document.querySelector("#logFilePath");
 
 connection.onopen = () => {
     connection.send("Hello from the client!");
