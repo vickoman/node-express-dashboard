210d0fed1a6b40282533977dae684b7e3739a2ee
vickoman <vicmandlagasca@gmail.com>
2022-01-08 20:01:57 -0500

    Add Logfile to root path

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { body } = require("express-validator");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Log Dashboard" });
+  res.render("index", { title: "Log Dashboard", logFile: req.query.logFile });
 });
 
 /* GET select file. */
